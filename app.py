d8e1c6544b18feb3a724443dea2e88a57f33dfe4
Soptorshi <bhattacharjeesoptorshi@gmail.com>
2022-03-07 18:30:05 +0530

    added status field in todo table and added login.html and register.html

--- app.py
+++ app.py
@@ -9,22 +9,48 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(100), nullable=False)
+    # completed = db.Column(db.Boolean)
+    completed = db.Column(db.String(20),nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow() )
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/', methods = ['POST','GET'])
+@app.route('/', methods=['POST','GET'])
+def entry_page():
+    return render_template('entry_page.html')
+
+@app.route('/login/', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    else:
+        return render_template('login.html')
+
+@app.route('/register/',methods = ['POST','GET'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+    else:
+        return render_template('register.html')
+
+
+@app.route('/home/', methods = ['POST','GET']) 
 def index():
     if request.method == 'POST':
         content = request.form['content']
-        new_task = Todo(content = content)
+        completed = request.form['completed']
+        new_task = Todo(content = content, completed = completed)
 
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect('/')
+            return redirect('/home/')
         except:
             return 'Error!'
     else:
@@ -37,7 +63,7 @@ def delete(id):
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect('/home/')
     except:
         return 'Error!'
 
@@ -47,9 +73,10 @@ def update(id):
 
     if request.method == 'POST':
         task.content = request.form['content']
+        task.completed = request.form['completed']
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect('/home/')
         except:
             return 'Error!'
     else:
